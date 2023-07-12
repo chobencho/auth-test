4dd235fea22e1b7d98daa081059a2a74448b8246
chobencho <chobencho@gmail.com>
2023-07-12 18:07:41 +0900

    add backend

---
+++ backend/app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+        include DeviseTokenAuth::Concerns::SetUserByToken
+
+        protect_from_forgery
+        helper_method :current_user, :user_signed_in?
+end
+      
