957f34a788631b8df1d4b2c2094e53c216fbef28
chobencho <119166395+chobencho@users.noreply.github.com>
2023-07-20 19:29:57 +0900

    Delete frontend directory

--- frontend/src/components/pages/MyBoard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyBoard = () => {
-  return (
-    <div>MyBoard</div>
-  )
-}
-
-export default MyBoard
--- frontend/src/components/pages/MyFav.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyFav = () => {
-  return (
-    <div>MyFav</div>
-  )
-}
-
-export default MyFav
